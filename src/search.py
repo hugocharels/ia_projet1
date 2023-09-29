6431e0ef0edd1a5aab1207ecde392b8de3c811c0
hugocharels <hugocharels@hotmail.com>
2023-09-29 11:21:59 +0200

    clamerd

--- src/search.py
+++ src/search.py
@@ -23,10 +23,11 @@ def are_opposites(action1: Action, action2: Action) -> bool:
 		   action1 == Action.WEST and action2 == Action.EAST or \
 		   action1 == Action.EAST and action2 == Action.WEST
 
-def is_useless(moves: Tuple[Action, ...], last_move: Tuple[Action, ...]) -> bool:
+def is_useless(moves: Tuple[Action, ...]) -> bool:
 	for i in range(len(moves)):
 		if moves[i] == Action.STAY: continue
-		if not are_opposites(moves[i], last_move[i]): return False
+		return False
+		#if not are_opposites(moves[i], last_move[i]): return False
 	return True
 
 
@@ -36,13 +37,15 @@ def search(problem: SearchProblem, Frontier: Generic) -> Optional[Solution]:
 	all_state = set()
 	while not frontier.empty():
 		state, actions, reward = frontier.get()
+		all_state.add(state)
+		print("----------------------")
+		print(state, actions, reward)
 		if problem.is_goal_state(state):
 			return Solution(actions=actions, reward=reward)
-		if state in all_state: continue
-		else: all_state.add(state)
 		for next_state, next_actions, next_reward in problem.get_successors(state):
-			#if next_state in all_state or (len(actions) > 0 and is_useless(next_actions, actions[-1])): continue
 			if next_state in all_state: continue
+			elif is_useless(next_actions): continue
+			else: all_state.add(state)
 			frontier.put((next_state, actions + [next_actions], next_reward + reward))
 	return None
 
@@ -58,17 +61,16 @@ def astar(problem: SearchProblem) -> Optional[Solution]:
 	all_state = set()
 	while not heap.isEmpty():
 		state, actions, reward = heap.pop()
-		if state in all_state: continue
-		else: all_state.add(state)
+		all_state.add(state)
 		print("----------------------")
 		print(state, actions, reward)
 		if problem.is_goal_state(state):
 			return Solution(actions=actions, reward=reward)
 		for next_state, next_actions, next_reward in problem.get_successors(state):
-			#if len(actions) > 0 and is_useless(next_actions, actions[-1]): continue
-			#if next_state in all_state: continue
+			print(next_actions)
+			if next_state in all_state: continue
+			elif is_useless(next_actions): continue
+			else: all_state.add(state)
 			heap.push((next_state, actions + [next_actions], next_reward + reward),
 					  next_reward + reward + problem.heuristic(next_state))
-			#		  next_reward + reward)
-			#		  reward + problem.heuristic(next_state))
 	return None
