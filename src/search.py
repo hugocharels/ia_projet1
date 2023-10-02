5737cfcd2b7256bf14abb67601f210ccd55bd187
Hugo <93338995+hugocharels@users.noreply.github.com>
2023-10-02 20:45:48 +0200

    Re indent

--- src/search.py
+++ src/search.py
@@ -38,25 +38,25 @@ def search(problem: SearchProblem, Frontier: type[Stack, Queue, Heap], is_useles
 	frontier.push(Node(None, problem.initial_state, None, 0))
 	visited = {problem.intinitial_state}
         while not frontier.is_empty():
-            node = frontier.pop()
-            if problem.is_goal_state(node.state):
-                return Solution(actions=node.get_actions())
-            for state, action, cost in problem.get_successors(node.state):
-                #if is_useless(node.action, action): continue
-                if state in visited: continue
-                visited.add(state)
-                next_node = Node(node, state, action, node.cost + problem.g(state, action, cost), node.cost + problem.f(state, action, cost))
-                 # if next_node in visited: continue
-                 # visited.add(next_node)
-                frontier.push(next_node)
-        return None
+		node = frontier.pop()
+		if problem.is_goal_state(node.state):
+			return Solution(actions=node.get_actions())
+		for state, action, cost in problem.get_successors(node.state):
+			#if is_useless(node.action, action): continue
+			if state in visited: continue
+			visited.add(state)
+			next_node = Node(node, state, action, node.cost + problem.g(state, action, cost), node.cost + problem.f(state, action, cost))
+		 	# if next_node in visited: continue
+		 	# visited.add(next_node)
+			frontier.push(next_node)
+	return None
 
 
-    def dfs(problem: SearchProblem) -> Optional[Solution]:
-        return search(problem, Stack)
+def dfs(problem: SearchProblem) -> Optional[Solution]:
+	return search(problem, Stack)
 
-    def bfs(problem: SearchProblem) -> Optional[Solution]:
-        return search(problem, Queue)
+def bfs(problem: SearchProblem) -> Optional[Solution]:
+	return search(problem, Queue)
 
-    def astar(problem: SearchProblem) -> Optional[Solution]:
-        return search(problem, Heap)
+def astar(problem: SearchProblem) -> Optional[Solution]:
+	return search(problem, Heap)
